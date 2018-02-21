39cdeac5f390b50c3917c458496a2ac85a611fcc
Grum <snips@MacBook-Pro-de-snips.local>
2018-02-21 16:52:22 +0100

    Add as week day from a date and remove useless files

--- snips-date-skill/datelogger/datelogger.py
+++ snips-date-skill/datelogger/datelogger.py
@@ -5,24 +5,28 @@ import calendar
 import locale
 from datetime import date
 
+mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre","Novembre","Décembre"]
+jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+
 class DateLogger:
     def __init__(self, locale=None):
         pass
     def sayDate(self):
         today = date.today()
-        jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-        mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre","Novembre","Décembre"]
         my_date = str(int(today.day))
         #print "Nous sommes le {} {} {}".format(jours[time.localtime()[6]], my_date, mois[time.localtime()[1]-1])
         return "Nous sommes le {} {} {}".format(jours[time.localtime()[6]], my_date, mois[time.localtime()[1]-1])
-    def sayDay(self):
-        jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+    def sayDay(self, year, month, dayNumber):
+        the_year = int(str(year))
+        the_month = mois.index(month) + 1
+        the_day = int(str(dayNumber))
         now = datetime.datetime(the_year, the_month, the_day)  #year,month,day
-        print now.weekday()
-        print jours[now.weekday()]
-        print now.day
+        return "Le {} {} {} est un {}".format(dayNumber, month, year, jours[now.weekday()])
 
 if  __name__ == "__main__":
+    print("hi")
     heysnips = DateLogger()
-    heysnips.sayDate()
-    heysnips.sa
+    """
+    heysnips.sayDate()®
+    heysnips.sayDay()
+    """
