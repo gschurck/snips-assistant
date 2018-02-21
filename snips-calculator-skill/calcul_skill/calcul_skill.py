ac375e7d0f1bde75c64717e1f0a24917d0618840
Grum <snips@MacBook-Pro-de-snips.local>
2018-02-21 12:45:56 +0100

    Updated addition skill

--- snips-calculator-skill/calcul_skill/calcul_skill.py
+++ snips-calculator-skill/calcul_skill/calcul_skill.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from math import sqrt
 
 def type_checker(number_to_check):
@@ -11,39 +13,40 @@ class Calculator:
     def addition(self, number1, number2):
         total = number1 + number2
         total = type_checker(total)
-        return "Le resultat est {}".format(total)
+        return "Le résultat est {}".format(total)
 
     def soustraction(self, number1, number2):
         total = number1 - number2
         total = type_checker(total)
-        return "Le resultat est {}".format(total)
+        return "Le résultat est {}".format(total)
 
     def division(self, number1, number2):
         total = number1 / number2
         total = type_checker(total)
-        return "Le resultat est {}".format(total)
+        return "Le résultat est {}".format(total)
 
     def multiplication(self, number1, number2):
         total = number1 * number2
         total = type_checker(total)
-        return "Le resultat est {}".format(total)
+        return "Le résultat est {}".format(total)
 
     def squared(self, number1):
         total = number1 * number1
         total = type_checker(total)
-        return "Le resultat est {}".format(total)
+        return "Le résultat est {}".format(total)
 
     def racine(self, number1):
         total = sqrt(number1)
         total = type_checker(total)
-        return "Le resultat est {}".format(total)
+        return "Le résultat est {}".format(total)
 
 if  __name__ == "__main__":
     heysnips = Calculator()
-
+    """
     print heysnips.addition(3,4)
     print heysnips.soustraction(2, 3)
     print heysnips.multiplication(2, 3)
     print heysnips.squared(3)
     #var = float(str(heysnips.racine(2))
     #print round(2.48687, 3)
+    """
